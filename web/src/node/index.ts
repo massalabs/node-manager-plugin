7e0defed36554e09c86ae4dce27c6b8db8985013
Peterjah <ps@massa.net>
2025-08-28 16:49:53 +0200

    add network to massastation network button

--- web/src/node/index.ts
+++ web/src/node/index.ts
@@ -2,6 +2,7 @@ export { NodeManager } from './NodeManager';
 export { Status } from './Status';
 export { OnOffBtn } from './OnOffBtn';
 export { Logs } from './Logs';
+export { MassaStationRpcButton } from './MassaStationRpcButton';
 export { useAutoRestart } from '../hooks/node-manager/useAutoRestart';
 export { useStartNode } from '../hooks/node-manager/useStartNode';
 export { useStopNode } from '../hooks/node-manager/useStopNode';
